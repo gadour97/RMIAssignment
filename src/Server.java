d9f412fb688eabb6acc4c52b3bffef1aa2e09aba
gadour <31381068+gadour97@users.noreply.github.com>
2021-12-08 16:24:17 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -3,12 +3,60 @@ import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-public class Server extends UnicastRemoteObject implements MyInterface {
+public class server extends UnicastRemoteObject implements MyInterface {
 
-    public Server() throws RemoteException {}
+    public server() throws RemoteException {}
+    @Override
+    public String sayHello() throws RemoteException {
+        return null;
+    }
+
+
+
+    public String reverse(String ch1){
+        String ch="";
+        for (int i=0;i<ch1.length();i++){
+            ch=ch1.charAt(i)+ch;
+        }
+        return ch;
+    }
+
+
+
+    public char minChar (String ch){
+        char min='z';
+        int a=ch.length();
+        for (int i=0; i<a-1; i++){
+            if (ch.charAt(i) < min)
+            { min = ch.charAt(i);}
+        }
+        return min;
+    }
+
+
+    public StringBuffer caseChanger (String ch){
+        StringBuffer newStr=new StringBuffer(ch);
+
+        for(int i = 0; i < ch.length(); i++) {
+            if(Character.isLowerCase(ch.charAt(i))) {
+                newStr.setCharAt(i, Character.toUpperCase(ch.charAt(i)));
+            }
+
+            else if(Character.isUpperCase(ch.charAt(i))) {
+                newStr.setCharAt(i, Character.toLowerCase(ch.charAt(i)));
+            }
+        }
+        return newStr;
+
+     }
+
+
+
+
+
 
     public static void main(String[] args) throws RemoteException, MalformedURLException {
-        Server server = new Server();
+        server server = new server();
 
         java.rmi.registry.LocateRegistry.createRegistry(1250);
 
